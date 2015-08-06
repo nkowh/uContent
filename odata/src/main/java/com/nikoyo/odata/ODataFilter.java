8152dc61954d7c0854d044890e9111694320cb43
QIliang <xiaoqlster@gmail.com>
2015-08-06 14:00:13 +0800

    加入ODataFilter

--- odata/src/main/java/com/nikoyo/odata/ODataFilter.java
+++ odata/src/main/java/com/nikoyo/odata/ODataFilter.java
@@ -6,13 +6,25 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SimpleCORSFilter implements Filter {
+public class ODataFilter implements Filter {
+
+
+	@Autowired
+	RequestContext context;
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		context.setRepositoryId(getRepositoryId(request));
+		context.setHttpServletRequest(request);
+
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,PATCH,MERGE");
@@ -20,9 +32,14 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		chain.doFilter(req, res);
 	}
-
+	private String getRepositoryId(final HttpServletRequest req) {
+		return req.getRequestURI().split("/")[1];
+	}
 	public void init(FilterConfig filterConfig) {}
 
 	public void destroy() {}
+
+
+
 
 }
