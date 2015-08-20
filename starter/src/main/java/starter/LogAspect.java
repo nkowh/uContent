e6cec0002d63a125fc4504d56bee9181de26ae4f
zht <taotao.gg@gmail.com>
2015-08-20 17:34:10 +0800

    添加日志信息用biulder转换为JSON格式

--- starter/src/main/java/starter/LogAspect.java
+++ starter/src/main/java/starter/LogAspect.java
@@ -7,10 +7,15 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import starter.service.LogService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -34,7 +39,7 @@ public class LogAspect {
         return getThreadLocal().get(key);
     }
 
-    private final String USER_NAME = "username";
+    private final String USER_NAME = "userName";
     private final String URL = "url";
     private final String IP_ADDRESS = "ipAddress";
 
@@ -43,17 +48,23 @@ public class LogAspect {
     private final String PARAM_NAMES = "paramNames";
 
     private final String START_TIME_MILLIS = "startTimeMillis";
-    private final String RESULT = "result";
-    private final String ERROR = "error";
+
+    private final String RESULT = "resultInfo";
+    private final String EX_MSG = "ex_msg";
+    private final String EX_STATUSCODE = "ex_statusCode";
+    private final String EX_STACKTRACE = "ex_stackTrace";
 
     private final String EXECUTION = "execution(public * starter.rest..*.*(..))";
+
     private final String SIMPLE_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
     private final String DECIMAL_FORMAT = ",###.###";
-
-    private final String MSG_NONE = "[无]";
+    private final String MSG_NONE = "";
 
     @Autowired
     private RequestContext context;
+
+    @Autowired
+    private LogService logService;
 
     @Pointcut(EXECUTION)
     public void recordLog() {
@@ -78,23 +89,25 @@ public class LogAspect {
             return result;
 
         } catch (Throwable ex) {
+            //获取本地线程变量
+            Map<String, Object> threadLocalMap = getThreadLocal();
+
             //记录异常信息
-            StringBuffer ex_sb = new StringBuffer();
-            ex_sb.append(ex + "\r\n");
+            threadLocalMap.put(EX_MSG, ex + "");
 
             //记录异常信息中http状态码
             if (ex instanceof uContentException) {
-                ex_sb.append("状态码：" + ((uContentException) ex).getStatusCode() + "\r\n");
+                threadLocalMap.put(EX_STATUSCODE, ((uContentException) ex).getStatusCode() + "");
             }
 
             //记录异常栈信息
+            StringBuffer ex_sb = new StringBuffer();
             for (StackTraceElement stackTraceElement : ex.getStackTrace()) {
                 ex_sb.append(stackTraceElement.toString() + "\r\n");
             }
+            threadLocalMap.put(EX_STACKTRACE, ex_sb.toString());
 
             //存入本地线程变量
-            Map<String, Object> threadLocalMap = getThreadLocal();
-            threadLocalMap.put(ERROR, ex_sb.toString());
             setThreadLocal(threadLocalMap);
 
             //抛出异常给业务逻辑处理
@@ -121,11 +134,11 @@ public class LogAspect {
         //从request获取相关信息
         HttpServletRequest request = context.getRequest();
 
-        String username = request.getUserPrincipal().getName();
-        threadLocalMap.put(USER_NAME, username);
-
         String url = request.getRequestURI();
         threadLocalMap.put(URL, url);
+
+        String username = request.getUserPrincipal().getName();
+        threadLocalMap.put(USER_NAME, username);
 
         String ipAddress = LogAspect.getIpAddress(request);
         threadLocalMap.put(IP_ADDRESS, ipAddress);
@@ -140,22 +153,22 @@ public class LogAspect {
 
 
     @After("recordLog()")
-    public void doAfter() {
+    public void doAfter() throws uContentException {
         //记录结束时间
         long endTimeMillis = System.currentTimeMillis();
-        String endTime = new SimpleDateFormat(SIMPLE_DATE_FORMAT).format(endTimeMillis);//格式化,为了展示使用
+        String endTime_format = new SimpleDateFormat(SIMPLE_DATE_FORMAT).format(endTimeMillis);//格式化,为了展示使用
 
         //计算耗时
         long startTimeMillis = (long) getThreadLocal(START_TIME_MILLIS);
-        String startTime = new SimpleDateFormat(SIMPLE_DATE_FORMAT).format(startTimeMillis);//格式化,为了展示使用
-        String time_consuming_string = new DecimalFormat(DECIMAL_FORMAT).format((endTimeMillis - startTimeMillis)) +
-                "ms";//格式化,为了展示使用
+        String startTime_format = new SimpleDateFormat(SIMPLE_DATE_FORMAT).format(startTimeMillis);//格式化,为了展示使用
+        long time_consuming = endTimeMillis - startTimeMillis;
+        String time_consuming_format = new DecimalFormat(DECIMAL_FORMAT).format(time_consuming) + "ms";//格式化,为了展示使用
 
         //从本地线程变量获取信息
         String username = (String) getThreadLocal(USER_NAME);
-        String url = (String) getThreadLocal(URL);
         String ipAddress = (String) getThreadLocal(IP_ADDRESS);
 
+        String url = (String) getThreadLocal(URL);
         String className = (String) getThreadLocal(CLASS_NAME);
         String methodName = (String) getThreadLocal(METHOD_NAME);
         String paramNames = MSG_NONE;
@@ -169,16 +182,64 @@ public class LogAspect {
             result_msg = result + "";
         }
 
-        String error_msg = MSG_NONE;
-        if (getThreadLocal().containsKey(ERROR)) {
-            error_msg = (String) getThreadLocal(ERROR);
+        String ex_msg = MSG_NONE;
+        if (getThreadLocal().containsKey(EX_MSG)) {
+            ex_msg = (String) getThreadLocal(EX_MSG);
+        }
+        String ex_statusCode = MSG_NONE;
+        if (getThreadLocal().containsKey(EX_STATUSCODE)) {
+            ex_statusCode = (String) getThreadLocal(EX_STATUSCODE);
+        }
+        String ex_stackTrace = MSG_NONE;
+        if (getThreadLocal().containsKey(EX_STACKTRACE)) {
+            ex_stackTrace = (String) getThreadLocal(EX_STACKTRACE);
+        }
+
+        //构建JSON格式对象
+        XContentBuilder builder;
+        try {
+            builder = XContentFactory.jsonBuilder();
+            builder.startObject()
+                    .field(USER_NAME, username)
+                    .field(IP_ADDRESS, ipAddress)
+                    .startObject("timeInfo")
+                    .field("start", startTimeMillis)
+                    .field("start_format", startTime_format)
+                    .field("end", endTimeMillis)
+                    .field("end_format", endTime_format)
+                    .field("consume", time_consuming)
+                    .field("consume_format", time_consuming_format)
+                    .endObject()
+                    .startObject("actionInfo")
+                    .field(URL, url)
+                    .field(CLASS_NAME, className)
+                    .field(METHOD_NAME, methodName)
+                    .field(PARAM_NAMES, paramNames)
+                    .endObject()
+                    .field(RESULT, result_msg)
+                    .startObject("exceptionInfo")
+                    .field(EX_MSG, ex_msg)
+                    .field(EX_STATUSCODE, ex_statusCode)
+                    .field(EX_STACKTRACE, ex_stackTrace)
+                    .endObject()
+                    .endObject();
+
+            System.out.println("builder is :" + builder.string());
+
+            XContentBuilder builder_return = logService.createLog(builder);
+
+            System.out.println("builder_return is :" + builder_return.string());
+
+        } catch (IOException e) {
+            //系统内部是否要记录异常log？
+            throw new uContentException(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         //展示结果
-        System.out.println("用户：" + username + "\nIP地址：" + ipAddress + "\n访问URL：" + url
-                + "\n开始时间：" + startTime + "\n结束时间：" + endTime + "\n耗时：" + time_consuming_string
-                + "\n目标类：" + className + "\n方法：" + methodName + "\n参数：" + paramNames
-                + "\n异常信息：" + error_msg + "\n返回信息：" + result_msg);
+        //System.out.println("用户：" + username + "\nIP地址：" + ipAddress + "\n访问URL：" + url
+        //        + "\n开始时间：" + startTime + "\n结束时间：" + endTime + "\n耗时：" + time_consuming_string
+        //        + "\n目标类：" + className + "\n方法：" + methodName + "\n参数：" + paramNames
+        //        + "\n异常信息：" + ex_msg + "\n返回信息：" + result_msg);
     }
 
     //获取远程IP的真实地址
@@ -207,5 +268,6 @@ public class LogAspect {
 
         return ip.equals("0:0:0:0:0:0:0:1") ? "127.0.0.1" : ip;
     }
+
 
 }
--- starter/src/main/java/starter/service/LogService.java
+++ starter/src/main/java/starter/service/LogService.java
@@ -1,17 +1,43 @@
 package starter.service;
 
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import starter.RequestContext;
 import starter.rest.Json;
+
+import java.io.IOException;
 
 @Service
 public class LogService {
     @Autowired
     private RequestContext context;
 
+    private final String LOG_TYPE_NAME = "logInfo";
+
     public XContentBuilder query(String type, Json parse, int start, int limit, String sort) {
         return null;
     }
+
+
+    public XContentBuilder createLog(XContentBuilder builder_in) throws IOException {
+        Client client = context.getClient();
+        IndexResponse indexResponse = client.prepareIndex(context.getIndex(), LOG_TYPE_NAME)
+                .setSource(builder_in).execute().actionGet();
+
+        XContentBuilder builder_out = XContentFactory.jsonBuilder();
+        builder_out.startObject()
+                .field("_index", indexResponse.getIndex())
+                .field("_type", indexResponse.getType())
+                .field("_id", indexResponse.getId())
+                .field("_version", indexResponse.getVersion())
+                .field("created", indexResponse.isCreated())
+                .endObject();
+
+        return builder_out;
+    }
+
 }
