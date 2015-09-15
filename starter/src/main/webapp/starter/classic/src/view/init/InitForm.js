b47348263ff6cc208680bf057c3bd001b6732120
QIliang <xiaoqlster@gmail.com>
2015-09-15 09:20:07 +0800

    补充

---
+++ starter/src/main/webapp/starter/classic/src/view/init/InitForm.js
@@ -0,0 +1,37 @@
+Ext.define('starter.view.init.InitForm', {
+    extend: 'Ext.form.Panel',
+    xtype: 'initForm',
+
+    title: '初始化',
+    frame: true,
+    width: 320,
+    bodyPadding: 10,
+
+    defaultType: 'numberfield',
+
+    items: [{
+        allowBlank: false,
+        fieldLabel: '分片',
+        bind:'{shards}'
+    }, {
+        allowBlank: false,
+        fieldLabel: '副本',
+        bind:'{replicas}'
+    }],
+
+    buttons: [
+        {
+            text: 'do',
+            handler: 'do'
+        }
+    ],
+
+    initComponent: function () {
+        this.defaults = {
+            anchor: '100%',
+            labelWidth: 120
+        };
+
+        this.callParent();
+    }
+});
