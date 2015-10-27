90e8002e4efbcb954133f37f435b49b7fcb85aa2
panw <panw201104011>
2015-10-27 14:35:59 +0800

    Merge branch 'master' of D:\git\uContent with conflicts.

--- starter/src/main/webapp/build/temp/development/starter/sass/config.rb
+++ starter/src/main/webapp/build/temp/development/starter/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('..\..\..\..\..\starter\sass\config.rb')
-cache_path = 'D:\ideaWork\uContent\starter\src\main\webapp/build/.sass-cache'
+cache_path = 'D:\git\uContent\starter\src\main\webapp/build/.sass-cache'
--- starter/src/main/webapp/build/temp/development/starter/sencha-compiler/app/full-page-master-bundle.js
+++ starter/src/main/webapp/build/temp/development/starter/sencha-compiler/app/full-page-master-bundle.js
@@ -1,2 +1,2 @@
 // @tag full-page
-// @require D:\ideaWork\uContent\starter\src\main\webapp\starter\app.js
+// @require D:\git\uContent\starter\src\main\webapp\starter\app.js
